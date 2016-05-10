21f1cf692319b37d48de22757699b88aa0a094b5
Panagiotis Nikitopoulos <panosbobos@yahoo.gr>
2016-05-10 23:39:31 +0300

    improved MyCompositeKeyComparator class performance

--- rtopk.hadoop/src/main/java/hadoopUtils/MyCompositeKeyComparator.java
+++ rtopk.hadoop/src/main/java/hadoopUtils/MyCompositeKeyComparator.java
@@ -1,6 +1,5 @@
 package hadoopUtils;
 
-import model.ItemType;
 import model.MyKey;
 
 import org.apache.hadoop.io.WritableComparable;
@@ -16,15 +15,19 @@ public class MyCompositeKeyComparator extends WritableComparator {
     	MyKey k1 = (MyKey)w1;
     	MyKey k2 = (MyKey)w2;
          
-    	if (k1.getKey().compareTo(k2.getKey()) != 0)
-			return k2.getKey().compareTo(k1.getKey());
+    	int tmp = k2.getKey().compareTo(k1.getKey());
+        
+    	if (tmp != 0)
+			return tmp;
+    	
+    	return -(k1.getType().getValue() - k2.getType().getValue());
 
-		if (k2.getType() == ItemType.S) {
+		/*if (k2.getType() == ItemType.S) {
 			if (k1.getType() == ItemType.W_InTopK) {
-				// Καθορίζει αν θα έρθουν πρώτα τα S ή τα W_InTopK.
+				// οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ S οΏ½ οΏ½οΏ½ W_InTopK.
 				return -1;
 			} else if (k1.getType() == ItemType.W) {
-				// Καθορίζει αν θα έρθουν πρώτα τα S ή τα W.
+				// οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ S οΏ½ οΏ½οΏ½ W.
 				return 1;
 			} else
 				return 0;// are same
@@ -34,21 +37,21 @@ public class MyCompositeKeyComparator extends WritableComparator {
 			} else if (k1.getType() == ItemType.W) {
 				;// are same, do nothing
 			} else {
-				// Καθορίζει αν θα έρθουν πρώτα τα S ή τα W.
+				// οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ S οΏ½ οΏ½οΏ½ W.
 				return -1;
 			}
 		} else {
 			if (k1.getType() == ItemType.W_InTopK) {
 				return 0;// are same
 			} else if (k1.getType() == ItemType.W) {
-				// Καθορίζει αν θα έρθουν πρώτα τα S ή τα W.
+				// οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ S οΏ½ οΏ½οΏ½ W.
 				return 1;
 			} else {
-				// Καθορίζει αν θα έρθουν πρώτα τα S ή τα W.
+				// οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½οΏ½οΏ½οΏ½ οΏ½οΏ½ S οΏ½ οΏ½οΏ½ W.
 				return 1;
 			}
 		}
 
-		return 0;
+		return 0;*/
     }
 }
